f6d0f60e36805e984f473757083b2b6d6f329e32
brian <brianhamiltonsf@gmail.com>
2015-02-05 21:14:07 -0800

    add employee model

---
+++ db/migrate/20150206051310_create_employees.rb
@@ -0,0 +1,22 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :firstname
+      t.string :string
+      t.string :lastname
+      t.string :phone
+      t.string :email
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :office_number
+      t.string :title
+      t.integer :manager_id
+      t.boolean :manager
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/employees.rb
@@ -0,0 +1,20 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :employee do
+    firstname "MyString"
+    string "MyString"
+    lastname "MyString"
+    phone "MyString"
+    email "MyString"
+    address1 "MyString"
+    address2 "MyString"
+    city "MyString"
+    state "MyString"
+    zip "MyString"
+    office_number "MyString"
+    title "MyString"
+    manager_id 1
+    manager false
+  end
+end
