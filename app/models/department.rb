e57c3872d8eb6d6d70e83e6fbb2cb0fea9690f43
brian <brianhamiltonsf@gmail.com>
2015-02-06 21:04:05 -0800

    add department views and supporting model methods

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,12 @@
 class Department < ActiveRecord::Base
   has_many :employees
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }
+
+  def get_owner
+    Employee.find(self.leader_id).name unless self.leader_id.nil?
+  end
+
+  def potential_owners
+    owners = Employee.find_by(manager && department_id == self.id)
+  end
 end
